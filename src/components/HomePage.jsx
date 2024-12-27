3b8ad361a8007f1aeab75a576e4768f2f159d193
beyza-yazici <beyzanyazici@hotmail.com>
2024-12-28 01:00:35 +0300

    homePage done v1

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "../css/HomePage.css"
+
+function HomePage() {
+  return (
+    <>
+    <div className="home-h1">
+        <h1>Teknolojik Yemekler</h1>
+    </div>
+    <div className='home-h2'> 
+    <h2>KOD ACIKTIRIR</h2> 
+    <h2>PİZZA, DOYURUR</h2>
+    </div>
+    <div className='home-button'>
+      <button> ACIKTIM </button>
+    </div>
+    </>
+  )
+}
+
+export default HomePage
